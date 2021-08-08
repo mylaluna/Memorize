02a8b7c5dc4abde6d0ca5cebfe86374a6ae5d0dd
Rain S. Zhang <mylaluna@outlook.com>
2021-08-08 18:07:10 +1000

    Adding a few protocols (lecture 6)

--- Memorize/EmojiMemoryGameView.swift
+++ Memorize/EmojiMemoryGameView.swift
@@ -18,20 +18,34 @@ struct EmojiMemoryGameView: View {
     // @ViewBuilder can also be used to mark a parameter of a function
     // parameter type "a function that returns a View"
     var body: some View {
-        ScrollView {
-            LazyVGrid(columns: [GridItem(.adaptive(minimum: 65))]) {
-                ForEach(game.cards) { card in
-                    CardView(card: card)
-                        .aspectRatio(2/3, contentMode: .fit)
-                        .onTapGesture {
-                            game.choose(card)
-                        }
-                }
+        AspectVGrid(items: game.cards, aspectRatio: 2/3) { card in
+            // following can be converted to be a function returns a ViewBuilder
+            if card.isMatched && !card.isFaceUp {
+                Rectangle().opacity(0)
+            } else {
+                CardView(card: card)
+                    .padding(4)
+                    .onTapGesture {
+                        game.choose(card)
+                    }
             }
         }
         .foregroundColor(.red)
         .padding(.horizontal)
     }
+    
+//    @ViewBuilder
+//    private func cardView(for card: EmojiMemoryGame.Card) -> some View {
+//        if card.isMatched && !card.isFaceUp {
+//            Rectangle().opacity(0)
+//        } else {
+//            CardView(card: card)
+//                .padding(4)
+//                .onTapGesture {
+//                    game.choose(card)
+//                }
+//        }
+//    }
 }
 
 struct CardView: View {
@@ -61,13 +75,12 @@ struct CardView: View {
     }
     
     private struct DrawingConstants {
-        static let cornerRadius: CGFloat = 20
+        static let cornerRadius: CGFloat = 10
         static let lineWidth: CGFloat = 3
-        static let fontScale: CGFloat = 0.8
+        static let fontScale: CGFloat = 0.75
         
     }
 }
-
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
